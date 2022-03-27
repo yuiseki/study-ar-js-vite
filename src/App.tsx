0af1d8934c448a4a2fb5ef6c0a4ca76fabf88cad
yuiseki <yuiseki@gmail.com>
2022-03-27 11:24:19 +0900

    おかしかったのを直す

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { registerComponent } from "aframe";
 import "@ar-js-org/ar.js";
 import { useEffect } from "react";
 
-registerComponent("a-marker", {});
-
 function App() {
   useEffect(() => {
     window.navigator.geolocation.getCurrentPosition((position) => {
@@ -17,32 +15,30 @@ function App() {
   }, []);
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Scene vr-mode-ui="enabled: false">
-        <a-scene
-          embedded
-          vr-mode-ui="enabled: false"
-          arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
-        >
-          <Entity
-            primitive="a-camera"
-            gps-camera="gpsMinDistance:10; minDistance:30; maxDistance:10000"
-            rotation-reader
-          ></Entity>
-          <a-text
-            value="test"
-            look-at="[gps-camera]"
-            scale="120 120 120"
-            gps-entity-place="latitude: 35.727593; longitude: 139.790962;"
-          ></a-text>
-          <a-text
-            value="Sky tree"
-            look-at="[gps-camera]"
-            scale="120 120 120"
-            gps-entity-place="latitude: 35.710063; longitude: 139.8107;"
-          ></a-text>
-        </a-scene>
-      </Scene>
+    <div>
+      <a-scene
+        embedded
+        vr-mode-ui="enabled: false"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+      >
+        <Entity
+          primitive="a-camera"
+          gps-camera="gpsMinDistance:10; minDistance:30; maxDistance:10000"
+          rotation-reader
+        ></Entity>
+        <a-text
+          value="test"
+          look-at="[gps-camera]"
+          scale="120 120 120"
+          gps-entity-place="latitude: 35.727593; longitude: 139.790962;"
+        ></a-text>
+        <a-text
+          value="Sky tree"
+          look-at="[gps-camera]"
+          scale="120 120 120"
+          gps-entity-place="latitude: 35.710063; longitude: 139.8107;"
+        ></a-text>
+      </a-scene>
     </div>
   );
 }
