1f0a8941f83cc0992b62e1f0b8da6d99d5c115d6
yuiseki <yuiseki@gmail.com>
2022-03-27 11:19:16 +0900

    add test

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,19 @@ function App() {
         <a-scene
           embedded
           vr-mode-ui="enabled: false"
-          arjs="sourceType: webcam; debugUIEnabled: false;"
+          arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
         >
           <Entity
             primitive="a-camera"
             gps-camera="gpsMinDistance:10; minDistance:30; maxDistance:10000"
             rotation-reader
           ></Entity>
+          <a-text
+            value="test"
+            look-at="[gps-camera]"
+            scale="120 120 120"
+            gps-entity-place="latitude: 35.727593; longitude: 139.790962;"
+          ></a-text>
           <a-text
             value="Sky tree"
             look-at="[gps-camera]"
