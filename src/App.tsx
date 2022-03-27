db7d740c3a1d14b4f19be3674aa66cf1d65e58ea
yuiseki <yuiseki@gmail.com>
2022-03-27 12:01:01 +0900

    dont use Entity

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Entity } from "aframe-react";
 import "babel-polyfill";
 import "@ar-js-org/ar.js";
 import { useEffect, useState } from "react";
@@ -18,38 +17,35 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <a-scene
-        embedded
-        vr-mode-ui="enabled: false"
-        arjs="sourceType: webcam; debugUIEnabled: false;"
-      >
-        <Entity
-          primitive="a-camera"
-          gps-camera="gpsMinDistance:10; minDistance:10; maxDistance:10000;"
-          rotation-reader
-        ></Entity>
-        {latitude && longitude && (
-          <a-box
-            material="color: yellow"
-            position="0 30 0"
-            gps-projected-entity-place={`latitude: ${latitude}; longitude: ${longitude};`}
-          ></a-box>
-        )}
-        <a-text
-          value="test"
-          look-at="[gps-camera]"
-          scale="200 200 200"
-          gps-entity-place="latitude: 35.727593; longitude: 139.790962;"
-        ></a-text>
-        <a-text
-          value="Sky tree"
-          look-at="[gps-camera]"
-          scale="300 300 300"
-          gps-entity-place="latitude: 35.710063; longitude: 139.8107;"
-        ></a-text>
-      </a-scene>
-    </div>
+    <a-scene
+      embedded
+      vr-mode-ui="enabled: false"
+      arjs="sourceType: webcam; debugUIEnabled: false;"
+    >
+      <a-camera
+        gps-camera="gpsMinDistance:10; minDistance:10; maxDistance:10000;"
+        rotation-reader
+      ></a-camera>
+      {latitude && longitude && (
+        <a-box
+          material="color: yellow"
+          position="0 30 0"
+          gps-projected-entity-place={`latitude: ${latitude}; longitude: ${longitude};`}
+        ></a-box>
+      )}
+      <a-text
+        value="test"
+        look-at="[gps-camera]"
+        scale="200 200 200"
+        gps-entity-place="latitude: 35.727593; longitude: 139.790962;"
+      ></a-text>
+      <a-text
+        value="Sky tree"
+        look-at="[gps-camera]"
+        scale="300 300 300"
+        gps-entity-place="latitude: 35.710063; longitude: 139.8107;"
+      ></a-text>
+    </a-scene>
   );
 }
 
